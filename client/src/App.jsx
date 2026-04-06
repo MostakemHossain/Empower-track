222727b1ea43d8899291e04fc7551ce4bdfd0058
MostakemHossain <mostakememon123@gmail.com>
2026-04-06 09:25:06 +0600

    routes created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+import { Toaster } from "react-hot-toast";
+import { Routes, Route, Navigate } from "react-router-dom";
+import LoginLanding from "./pages/LoginLanding";
+import Layout from "./pages/Layout";
+import Dashboard from "./pages/Dashboard";
+import Settings from "./pages/Settings";
+import PaySlips from "./pages/PaySlips";
+import Leave from "./pages/Leave";
+import Attendance from "./pages/Attendance";
+import Employees from "./pages/Employees";
+import PrintPaySlip from "./pages/PrintPaySlip";
 
 const App = () => {
   return (
-    <div className=''>App</div>
-  )
-}
+    <>
+      <Toaster />
+      <Routes>
+        <Route path="/login" element={<LoginLanding />} />
+        <Route element={<Layout />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/employees" element={<Employees />} />
+          <Route path="/attendance" element={<Attendance />} />
+          <Route path="/leave" element={<Leave />} />
+          <Route path="/payslips" element={<PaySlips />} />
+          <Route path="/settings" element={<Settings />} />
+        </Route>
 
-export default App
+        <Route path="/print/payslips/:id" element={<PrintPaySlip />} />
+
+        <Route path="*" element={<Navigate to={"/dashboard"} replace />} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
