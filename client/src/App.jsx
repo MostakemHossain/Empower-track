ce1de68eeaf5e9953ec6370479f12c542a0804c0
MostakemHossain <mostakememon123@gmail.com>
2026-04-13 10:13:12 +0600

    login landing page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ import Leave from "./pages/Leave";
 import Attendance from "./pages/Attendance";
 import Employees from "./pages/Employees";
 import PrintPaySlip from "./pages/PrintPaySlip";
+import LoginForm from "./components/LoginForm";
 
 const App = () => {
   return (
@@ -17,6 +18,21 @@ const App = () => {
       <Toaster />
       <Routes>
         <Route path="/login" element={<LoginLanding />} />
+        <Route
+          path="/login/admin"
+          role="admin"
+          title="Admin Portal"
+          subTitle="Sign in to manage the organization"
+          element={<LoginForm />}
+        />
+        <Route
+          path="/login/employee"
+          role="employee"
+          title="Employee Portal"
+          subTitle="Sign in to access your"
+          element={<LoginForm />}
+        />
+
         <Route element={<Layout />}>
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/employees" element={<Employees />} />
