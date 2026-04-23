4f412f5ecdc23324f3fb82850480be1ce1aa903a
MostakemHossain <mostakememon123@gmail.com>
2026-04-23 22:23:59 +0600

    create employee route

--- server/app.js
+++ server/app.js
@@ -1,8 +1,8 @@
 import express from "express";
 import cors from "cors";
-import multer from "multer";
 import dotenv from "dotenv";
-
+import router from "./router/index.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
@@ -10,13 +10,18 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
-app.use(multer().none());
-dotenv.config()
+dotenv.config();
+app.use(cookieParser());
+
+
+app.use('/api/v1', router);
 
 // Routes
 app.get("/", (req, res) => {
   res.send("API is running 🚀");
 });
+
+
 
 app.get("/test", (req, res) => {
   res.send("Hello World!");
---
+++ server/routes/employee-routes.js
@@ -0,0 +1,17 @@
+import express from "express";
+import { EmployeeController } from "../controller/employee-controller.js";
+import { fileUploader } from '../shared/fileUpload.js';
+const router = express.Router();
+
+router.post(
+  "/create-employee",
+  //   auth("admin", "super-admin"),
+  fileUploader.upload.single("file"),
+  (req, res, next) => {
+    req.body = JSON.parse(req.body?.data);
+    return EmployeeController.createEmployee(req, res, next);
+  }
+);
+
+const EmployeeRouter = router;
+export default EmployeeRouter;
